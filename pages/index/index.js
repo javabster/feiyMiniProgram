45bd5d81cb4cc8fa760350853df68d7765d38ca5
Abby Mitchell <abby.mitchell.15@ucl.ac.uk>
2018-04-23 06:34:56 +0100

    Added invalid password modal

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,6 +15,20 @@ Page({
     var email = e.detail.value.email
     var password = e.detail.value.password
     var passwordCon = e.detail.value.passwordCon
+
+    if (password !== passwordCon) {
+      wx.showModal({
+        title: "Invalid Password Input",
+        content: "Your passwords don't match, please try again",
+        confirmText: "Ok",
+        showCancel: false,
+        success: function (res) {
+          console.log('success')
+        }
+      })
+    } else {
+      // database stuff
+    }
   }
 
 })
